73bbfaa8787004d66c0f47439eab58fe5ed0002c
zhouyuqin123 <zhouyuqin@storswift.com>
2023-12-05 20:03:54 +0800

    feat：Added multiple interface definitions

--- packages/core/base/signer.ts
+++ packages/core/base/signer.ts
@@ -17,19 +17,25 @@ export abstract class BaseSignerWalletAdapter<Name extends string = string>
 export interface MessageSignerWalletAdapterProps<Name extends string = string> extends WalletAdapterProps<Name> {
     signMessage(message: Uint8Array): Promise<Uint8Array>;
 
-    requestViewKey(): Promise<string>;
-
     decrypt(cipherText: string, tpk?: string, programId?: string, functionName?: string, index?: number): Promise<string>;
 
     requestRecords(program: string): Promise<any[]>;
 
     requestTransaction(transaction: AleoTransaction): Promise<string>;
 
+    requestExecution(transaction: AleoTransaction): Promise<string>;
+
     requestBulkTransactions(transactions: AleoTransaction[]): Promise<string[]>;
 
     requestDeploy(deployment: AleoDeployment): Promise<string>;
 
     transactionStatus(transactionId: string): Promise<string>;
+
+    getExecution(transactionId: string): Promise<string>;
+
+    requestRecordPlaintexts(program: string): Promise<any[]>;
+
+    requestTransactionHistory(program: string): Promise<any[]>;
 }
 
 export type MessageSignerWalletAdapter<Name extends string = string> = WalletAdapter<Name> &
@@ -41,17 +47,23 @@ export abstract class BaseMessageSignerWalletAdapter<Name extends string = strin
 {
     abstract signMessage(message: Uint8Array): Promise<Uint8Array>;
 
-    abstract requestViewKey(): Promise<string>;
-
     abstract decrypt(cipherText: string, tpk?: string, programId?: string, functionName?: string, index?: number): Promise<string>;
 
     abstract requestRecords(program: string): Promise<any[]>;
 
     abstract requestTransaction(transaction: AleoTransaction): Promise<string>;
 
+    abstract requestExecution(transaction: AleoTransaction): Promise<string>;
+
     abstract requestBulkTransactions(transactions: AleoTransaction[]): Promise<string[]>;
 
     abstract requestDeploy(deployment: AleoDeployment): Promise<string>;
 
     abstract transactionStatus(transactionId: string): Promise<string>;
+
+    abstract getExecution(transactionId: string): Promise<string>;
+
+    abstract requestRecordPlaintexts(program: string): Promise<any[]>;
+
+    abstract requestTransactionHistory(program: string): Promise<any[]>;
 }
